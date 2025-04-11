52a948f175e2d33a83960dfefca540ef88f3a79f
Funkasan <158597679+Funkasan@users.noreply.github.com>
2025-04-11 19:11:54 +0300

    Add files via upload

---
+++ baigiamasis_projektas/src/main/java/org/example/fe/de/baigiamasis_projektas/BaigiamasisProjektasApplication.java
@@ -0,0 +1,13 @@
+package org.example.fe.de.baigiamasis_projektas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaigiamasisProjektasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaigiamasisProjektasApplication.class, args);
+    }
+
+}
---
+++ baigiamasis_projektas/src/test/java/org/example/fe/de/baigiamasis_projektas/BaigiamasisProjektasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fe.de.baigiamasis_projektas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaigiamasisProjektasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
